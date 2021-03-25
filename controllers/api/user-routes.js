34ab250df784ed8ee19e46f906ef20755a8e4de5
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-25 18:43:26 -0500

    added user get all, get one, and post routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,33 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../../models/index');
 
 router.get('/', (req, res) => {
-    res.json({ message: 'API'});
+    User.findAll({
+        attributes: {
+            exclude: ['password']
+        }
+    }).then(data => {
+        res.json(data);
+    })
+});
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data));
+})
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    }).then(data => {
+        res.json(data);
+    })
 });
 
 module.exports = router;
