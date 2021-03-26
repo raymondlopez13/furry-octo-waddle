49c1f172d93c279802d7ab39462ebb214b8068e4
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-25 22:13:09 -0500

    Added comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/', (req,res) => {
+    Comment.findAll().then(data => res.json(data));
+});
+
+router.post('/', (req,res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    }).then(data => res.json(data));
+});
+
+router.delete('/:id', (req,res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data));
+});
+
+module.exports = router;
