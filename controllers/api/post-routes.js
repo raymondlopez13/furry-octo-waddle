68fcc4545b9194f587e8df4d0c4d24481c5fa2cb
Raymond Lopez <raymondlopez13@yahoo.com>
2021-03-25 21:58:39 -0500

    Finished basic post routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models/index');
+
+router.get('/', (req,res) => {
+    Post.findAll({
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(data => res.json(data));
+});
+
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(data => res.json(data));
+});
+
+router.post('/', (req,res) => {
+    Post.create({
+        title: req.body.title,
+        post_url: req.body.post_url,
+        user_id: req.body.user_id
+    }).then(data => res.json(data));
+});
+
+router.put('/:id', (req,res) => {
+    Post.update({
+        title: req.body.title
+    },
+     {
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data));
+});
+
+router.delete('/:id', (req,res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(data => res.json(data));
+})
+
+module.exports = router;
